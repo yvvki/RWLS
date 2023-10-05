645b398d2fd93ee9ba6a7a2a69dcfc6e21a4481a
Yuuki Rika <yvvki@duck.com>
2023-10-05 09:39:00 +0700

    Refactor
    - Removed unused `Closable` interface
    - Pull `supplyAsync` up to the whole `initialize` function
    - Import and code cleanup

--- server/src/main/kotlin/RustedWarfareLanguageServer.kt
+++ server/src/main/kotlin/RustedWarfareLanguageServer.kt
@@ -1,23 +1,25 @@
 import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.jsonrpc.messages.Either
-import org.eclipse.lsp4j.services.*
-import java.io.Closeable
+import org.eclipse.lsp4j.services.LanguageClient
+import org.eclipse.lsp4j.services.LanguageClientAware
+import org.eclipse.lsp4j.services.LanguageServer
 import java.util.concurrent.CompletableFuture
 
-class RustedWarfareLanguageServer : LanguageServer, LanguageClientAware, Closeable {
+class RustedWarfareLanguageServer : LanguageServer, LanguageClientAware {
     private val textDocumentService = RustedWarfareTextDocumentService()
     private val workspaceService = RustedWarfareWorkspaceService()
-    
+
     private var client: LanguageClient? = null
-    
-    override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> {
-        val capabilities = ServerCapabilities().apply { 
-            textDocumentSync = Either.forLeft(TextDocumentSyncKind.Full)
-            completionProvider = CompletionOptions()
+
+    override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> =
+        CompletableFuture.supplyAsync {
+            val capabilities = ServerCapabilities().apply {
+                textDocumentSync = Either.forLeft(TextDocumentSyncKind.Full)
+                completionProvider = CompletionOptions()
+            }
+
+            InitializeResult(capabilities)
         }
-        
-        return CompletableFuture.supplyAsync { InitializeResult(capabilities) }
-    }
 
     override fun shutdown(): CompletableFuture<Any> {
         TODO("Not yet implemented")
@@ -34,7 +36,4 @@ class RustedWarfareLanguageServer : LanguageServer, LanguageClientAware, Closeab
     override fun connect(client: LanguageClient?) {
         this.client = client
     }
-
-    override fun close() {}
-
 }
